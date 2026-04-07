7c13867f4536974625332b929c8035cf590412d7
Matt Burton <matthewwburton@gmail.com>
2026-04-07 14:01:15 +0200

    version upgrade

--- Flail Quail Unity/Assets/Code/GameManager.cs
+++ Flail Quail Unity/Assets/Code/GameManager.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public GameObject gameOverMenu;
+
+    int score = 0;
+    public TextMeshProUGUI scoreText;
 
     private void Awake()
     {
@@ -23,5 +27,11 @@ public class GameManager : MonoBehaviour
     public void Restart()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void AddPoint()
+    {
+        score++;
+        scoreText.SetText(score.ToString());
     }
 }
