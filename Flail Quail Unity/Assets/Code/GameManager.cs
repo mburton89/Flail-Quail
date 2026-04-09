437e80e86f387d6dab5b663531e906f2ff5636a8
Matt Burton <matthewwburton@gmail.com>
2026-04-09 16:39:10 +0200

    prey por la classe

--- Flail Quail Unity/Assets/Code/GameManager.cs
+++ Flail Quail Unity/Assets/Code/GameManager.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine;
+using UnityEngine.Localization;           // for LocalizedString
+using UnityEngine.Localization.Settings;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,6 +14,7 @@ public class GameManager : MonoBehaviour
 
     int score = 0;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI bestScoreText;
 
     private void Awake()
     {
@@ -37,5 +41,49 @@ public class GameManager : MonoBehaviour
     {
         score++;
         scoreText.SetText(score.ToString());
+    }
+
+    void CheckForNewBestScore()
+    {
+        string localizedBestScoreLabel;
+
+        // Get the currently active locale from the Localization package
+        Locale currentLocale = LocalizationSettings.SelectedLocale;
+
+        if (currentLocale != null)
+        {
+            string localeCode = currentLocale.Identifier.Code;   // e.g. "fr", "en", "de-CH", "de"
+
+            switch (localeCode)
+            {
+                case "fr":
+                    localizedBestScoreLabel = "Meilleur score : ";
+                    break;
+
+                case "de":     // German / Swiss German
+                case "de-CH":  // Explicit Swiss German if you have a custom locale
+                    localizedBestScoreLabel = "Beste Punktzahl: ";
+                    break;
+
+                case "en":
+                default:
+                    localizedBestScoreLabel = "Best score: ";
+                    break;
+            }
+        }
+        else
+        {
+            // Fallback in case localization hasn't initialized yet
+            localizedBestScoreLabel = "Best score: ";
+        }
+
+        int currentBestScore = PlayerPrefs.GetInt("BestScore");
+        
+        if (score > currentBestScore)
+        {
+            PlayerPrefs.SetInt("BestScore", score);
+        }
+
+        bestScoreText.SetText(localizedBestScoreLabel + PlayerPrefs.GetInt("BestScore"));
     }
 }
